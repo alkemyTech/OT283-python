de91896f3cfefbbf32aa4c40cc6bc02c9dcdb777
JoseSalas98 <josesalasbolivar98@gmail.com>
2022-08-28 18:28:58 -0500

    OT283-51: Se añade la función para extraer datos a partir de los scripts .sql

--- sprint02/etl_dag.py
+++ sprint02/etl_dag.py
@@ -1,8 +1,8 @@
 # Importamos las dependencias necesarias
-from connect_dag import start_connct
+from sprint01.connect_dag import start_connct
 from confi import db_user
 from constans import LOG_DIR, SCHEMA_NAME
-from elt_funtions import get_table, transform_dts
+from sprint02.elt_functions import get_table, transform_dts
 
 import logging
 from datetime import datetime
@@ -43,8 +43,29 @@ default_args = {
                 "retry_delay": timedelta(minutes = 5),
                 }
 
-def get_data():
-    logging.info(f"getting the data from {table_name}")
+#       Creamos las funciones que intervendrán en el proceso
+##      Declaramos las inputs de la función
+schema_name = SCHEMA_NAME                               ##  get_data(schema_name, engine)
+engine = start_connct()                                 
+
+##      Definimos los agumentos del python callable           
+get_data_kwargs = {
+                    "schema_name" : schema_name,
+                    "engine" : engine 
+                    }
+
+def get_data(
+                schema_name, 
+                engine
+            ):
+    extract_path_dict = {}
+    for table_name in schema_name:
+        extract_path_dict[f"{table_name}"] = get_table(
+                                                        table_name, 
+                                                        engine
+                                                        )
+        logging.info(f"getting the data from {table_name}")
+    return extract_path_dict
 
 def transform_data():
     logging.info(f"transforming the data from {table_name}")
@@ -64,6 +85,7 @@ with DAG(
         get_data_task = PythonOperator(
                                         task_id = "get_data", 
                                         python_callable = get_data,
+                                        op_kwarg = get_data_kwargs,
                                         retries = 5, 
                                         retry_delay = timedelta(minutes=5),
                                         dag = dag
