37567db0e7b4fa0bb86cbadce016bde611cd2c19
Ale Gomez <alejandro.gomez969@gmail.com>
2022-08-29 10:42:17 -0300

    tikets OT283-37 y OT283-45, creacion de logs y creacion de python operator extraccion informacion de base ded atos usando SQL

--- dags/db_connections.py
+++ dags/db_connections.py
@@ -1,12 +1,22 @@
 import logging
 from datetime import datetime, timedelta
+import pandas as pd
+from pathlib import Path
+from decouple import config
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 import time
 from sqlalchemy import exc, create_engine, inspect
-from decouple import config
 
-DB_DATA_CONNECT = "postgresql://username:password@host:port/database"
+# Tomamos la fecha actual y el directorio raiz para armar la ruta de la carpeta que contendra los logs de el dag
+ROOT_DIR = Path('../')
+today = datetime.now()
+# Traemos de .env las credenciales para la base de datos
+DB_USER = config('DB_USER')
+PASSWORD = config('PASSWORD')
+HOST = config('HOST')
+PORT = config('PORT')
+DB_NAME = config('DB_NAME')
 
 def db_connection():
     """
@@ -14,35 +24,39 @@ def db_connection():
         * Definimos un bucle para controlar que se ejecute solamente si los intentos son menor o igual a 5
         o existen las tablas en la base de datos
     """
-    flag = True
+    flag = False
     count = 0
     logging.info("Conectando a la base de datos")
-    while flag and count < 5:
+    while not flag and count < 5:
         logging.info(f"Intento N°{count}")
         try:
             # Establezco la coneccion mendiante la variable de entorno que contiene el enlace a la db postgre
-            engine = create_engine(config('DB_DATA_CONNECT'))
-            engine.connect()
-            inspec = inspect(engine)
+            engine = create_engine(f"postgresql://{DB_USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}")
+            conn = engine.connect()
+            logging.info("Conexion a la base de datos realizada exitosamente")
 
             # Vemos que las tablas existan para ver si se vuelve a intentar conectar o no
-            if inspec.has_table("flores_comahue") and inspec.has_table('salvador_villa_maria'):
-                logging.info("No existen las tablas en la base de datos")
-                flag=False
+            if engine.dialect.has_table(conn,"flores_comahue") and engine.dialect.has_table(conn, 'salvador_villa_maria'):  
+                logging.info("Las tablas existen")
+                flag=True
             else:
                 # En caso de que no se encuentren las tablas se incrementa la variable contados de control y se hace un sleep de 30 segundos entre un intento y otros para evitar delays de conexion
+                logging.info("No existen las tablas en la base de datos")
                 count = count + 1
                 time.sleep(30)
+            
         except exc.SQLAlchemyError:
             # Se incrementa la variable contador de control en caso de encontrar un error, y de igual manera se espera 30 segundo antes del siguiente reintegro
             count = count + 1
             time.sleep(30)
+
     return engine
 
 logger = logging.getLogger('Universidades A')
 
 # Configuracion del loggs
 logging.basicConfig(
+    filename= f"{ROOT_DIR}../logs/retry-{today}.log",
     level=logging.INFO,
     datefmt='%Y-%M-%D',
     format='%(asctime)s- %(logger)s - %(mensaje)s'
@@ -61,6 +75,7 @@ with DAG(
     schedule_interval=timedelta(days=1),
     start_date=datetime(2022,8,23),
 ) as dag:
+
     task_check_db_connection = PythonOperator(
         task_id = 'check_db_connection',
         python_callable = db_connection,
--- dags/etl.py
+++ dags/etl.py
@@ -1,16 +1,15 @@
 from airflow import DAG
 from datetime import timedelta, datetime
-from airflow.operators.python_operator import PythonOperator
+from sqlalchemy import create_engine
+from airflow.operators.python import PythonOperator
 import logging
+from funciones_etl.extract_data import extract_db
+from funciones_etl.process_data import process_data_uni
+from funciones_etl.upload_data import upload_to_s3
 
-def extract_from_db():
-    pass
 
-def transform_data_uni():
-    pass
 
-def load_to_s3():
-    pass
+logger = logging.getLogger('Universidades A')
 
 # Configuracion del loggs
 logging.basicConfig(
@@ -19,6 +18,7 @@ logging.basicConfig(
     format='%(asctime)s- %(logger)s - %(mensaje)s'
 )
 
+# Configuramos los default arguments para airflow
 default_args = {
     'owner':'Airflow',
     'retries':1,
@@ -26,7 +26,7 @@ default_args = {
 }
 
 with DAG(
-    'dag_OT283-21',
+    'dag_OT283-ETL',
     description='Configuracion DAG para Univeridades del grupo A',
     default_args=default_args,
     schedule_interval=timedelta(hours=1),
@@ -38,23 +38,27 @@ with DAG(
     # Tareas que se dejan armadas para luego ser utilizadas mas adelante en el ETL
     extract = PythonOperator(
         task_id = 'extract',
-        python_callable = extract_from_db,
+        python_callable = extract_db,
         retries=5,
         retry_delay=timedelta(minutes=5),
+        op_args=['flores.sql','villaMaria.sql'],
         dag=dag
     )
 
     transform = PythonOperator(
         task_id='transform',
-        python_callable=transform_data_uni,
+        python_callable=process_data_uni,
         dag=dag
     )
 
     load = PythonOperator(
         task_id='load',
-        python_callable=load_to_s3,
+        python_callable=upload_to_s3,
         dag=dag
     )
 
     # Flujo de ejecucion
     extract >> transform >> load
+
+# if __name__ == '__main__':
+#     extract_db('flores.sql','villaMaria.sql')
